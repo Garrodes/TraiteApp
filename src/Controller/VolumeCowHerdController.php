6cbef7fce917ee6b8bbfc987f629d6f6f9e8dc18
Garrodes <batuque77mlh@gmail.com>
2023-04-29 14:40:02 +0200

    🐛 FIX: template

--- src/Controller/VolumeCowHerdController.php
+++ src/Controller/VolumeCowHerdController.php
@@ -4,20 +4,32 @@ namespace App\Controller;
 
 use App\Entity\VolumeCowHerd;
 use App\Repository\VolumeCowHerdRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VolumeCowHerdController extends AbstractController
 {
-    #[Route('/milk', name: 'app_volume_cow_herd')]
-    public function index( VolumeCowHerdRepository $repository): Response
+    /**
+     * This function display all herd's milk volumes by group
+     *
+     * @param VolumeCowHerdRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/milk', name: 'app_volume_cow_herd', methods: ('GET'))]
+    public function index( VolumeCowHerdRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
-        $volumes = $repository -> findAll() ;
+        $milkVolumes = $paginator->paginate(
+            $repository -> findAll(), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10); /*limit per page*/
 
-     
         return $this->render('pages/milk.html.twig', [
-            'volumes' => $volumes,
+            'milkVolumes' => $milkVolumes,
         ]);
     }
 }
